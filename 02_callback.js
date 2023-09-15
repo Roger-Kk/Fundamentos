c7a48225fcbae457d70fb4a835dc91c58ea0e9ed
Roger <rogerkolossoski@gmail.com>
2023-09-15 09:49:27 -0300

    Update 02_callback.js

--- 02_callback.js
+++ 02_callback.js
@@ -1,4 +1,27 @@
-// Callback functions
+//Callback function é uma função que tem como parâmetro outra função: 
+function print(callback) {  
+    callback();
+}
+
+//Exemplo: A função setTimeout tem a const mensagem como parâmetro que é uma função que exibe msg na tela. 
+setTimeout(mensagem, 3000);
+
+const mensagem = function() {  
+    console.log("Essa mensagem é exibida depois de 3 segundos");
+}
+
+//Como alternativa podemos escrever a função dentro do parâmetro da outra função ao invés de chamá-la: 
+setTimeout(function() {  
+    console.log("Essa mensagem é exibida após 3 segundos");
+}, 3000);
+
+//Ainda podemos usar uma arrow function (ES6) para escrever a função que vai como parâmetro: 
+setTimeout(() => { 
+    console.log("Essa mensagem é exibida após 3 segundos");
+}, 3000);
+
+
+// Examples: 
 // forEach chamando uma função declarada fora dela:
 // forEach tem como parâmetro obrigatoriamente uma função
 
@@ -24,7 +47,7 @@ console.log(notasAtualizadas);
 //Operador ternário:  condição ? true : false
                  // nota == 10 ? nota : ++nota
                  
-const notasAtualizadas2 =notas.map( nota => nota ==10 ? nota : ++nota) ;
+const notasAtualizadas2 = notas.map( nota => nota ==10 ? nota : ++nota) ;
 console.log(notasAtualizadas2);
 
 
